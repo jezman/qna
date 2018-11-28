ac75493a7434b90ca249b440ad74d23f8407d248
jezman <dev.jezman@gmail.com>
2018-11-28 13:50:28 +0300

    Questions controller PATCH #update.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :find_question, only: %i[show edit]
+  before_action :find_question, only: %i[show edit update]
   def index
     @questions = Question.all
   end
@@ -21,6 +21,14 @@ class QuestionsController < ApplicationController
       redirect_to @question
     else
       render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
     end
   end
 
