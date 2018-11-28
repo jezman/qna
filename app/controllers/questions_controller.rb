6a0e0d89e018edc2a62bd5a7ad5e406790d32d1a
jezman <dev.jezman@gmail.com>
2018-11-28 13:32:33 +0300

    Questions controller: GET #edit.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,13 @@
 class QuestionsController < ApplicationController
+  before_action :find_question, only: %i[show edit]
   def index
     @questions = Question.all
   end
 
   def show
-    @question = Question.find(params[:id])
+  end
+
+  def edit
   end
 
   def new
@@ -22,6 +25,10 @@ class QuestionsController < ApplicationController
   end
 
   private
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
 
   def question_params
     params.require(:question).permit(:title, :body)
