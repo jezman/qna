a5603907d5a7870e5894f1704f40fddbc45d769a
jezman <dev.jezman@gmail.com>
2018-11-28 15:01:47 +0300

    Answer controller: GET #new.

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  describe 'GET #new' do
+    let(:question) { create(:question) }
+    let(:answer) { create(:answer, question: question) }
 
+    before { get :new, params: { question_id: question } }
+
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
 end
