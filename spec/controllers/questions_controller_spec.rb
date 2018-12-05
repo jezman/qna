3842a6e1ff5825b1666eabd9c42e2fd8a99908d9
jezman <dev.jezman@gmail.com>
2018-12-05 12:22:31 +0300

    Only author can remove question.

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -124,15 +124,34 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
-    let!(:question) { create(:question) }
+    context 'user an author' do
+      before { login(user) }
+      before { question }
 
-    it 'deletes the question' do
-      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+      it 'delete the question' do
+        expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+      end
+
+      it 'redirects to index' do
+        delete :destroy, params: { id: question }
+        expect(response).to redirect_to questions_path
+      end
     end
 
-    it 'redirects to index' do
-      delete :destroy, params: { id: question }
-      expect(response).to redirect_to questions_path
+    context 'user is not an author' do
+      let!(:not_user) { create(:user) }
+
+      before { login(not_user) }
+      before { question }
+
+      it 'delete the question' do
+        expect { delete :destroy, params: { id: question } }.to_not change(Question, :count)
+      end
+
+      it 'redirects to question show' do
+        delete :destroy, params: { id: question }
+        expect(response).to redirect_to question_path(question)
+      end
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :password }
 
   it { should have_many :questions }
+
+  describe 'user is author' do
+    let(:user) { create(:user) }
+    let(:any_user) { create(:user) }
+
+    it 'current user is author' do
+      question = create(:question, user: user)
+
+      expect(user).to be_author(question)
+    end
+
+    it 'current user not an author' do
+      question = create(:question, user: any_user)
+
+      expect(user).to_not be_author(question)
+    end
+  end
 end
