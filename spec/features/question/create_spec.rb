54da7f0da54173a9e7fe929562e9dc19b38147e4
jezman <dev.jezman@gmail.com>
2018-12-15 16:33:47 +0300

    User can add one or more files when you create a question.

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -5,38 +5,49 @@ feature 'User can create question', %q{
   As an authenticated user
   I'd like to be able to ask the question
 } do
-  given(:user) { create(:user) }
+  given!(:user) { create(:user) }
 
-  scenario 'Authenticated user asks a question' do
-    sign_in(user)
+  describe 'Authenticated user' do
+    before { sign_in(user)}
 
-    visit questions_path
-    click_on 'New question'
+    before do
+      visit questions_path
+      click_on 'New question'
+    end
 
-    fill_in 'Title', with: 'Question title'
-    fill_in 'Body', with: 'Question body'
-    click_on 'Ask'
+    scenario 'asks a question' do
+      fill_in 'Title', with: 'Question title'
+      fill_in 'Body', with: 'Question body'
+      click_on 'Ask'
 
-    expect(page).to have_content 'Your question successfully created.'
-    expect(page).to have_content user.questions.last.title
-    expect(page).to have_content user.questions.last.body
-  end
+      expect(page).to have_content 'Your question successfully created.'
+      expect(page).to have_content user.questions.last.title
+      expect(page).to have_content user.questions.last.body
+    end
 
-  scenario 'Authenticated user asks a question with errors' do
-    sign_in(user)
+    scenario 'asc question with attach files' do
+      fill_in 'Title', with: 'Question title'
+      fill_in 'Body', with: 'Question body'
 
-    visit questions_path
-    click_on 'New question'
-    click_on 'Ask'
+      attach_file 'File', ["#{Rails.root.join('spec/rails_helper.rb')}", "#{Rails.root.join('spec/spec_helper.rb').to_s}"]
+      click_on 'Ask'
 
-    expect(page).to have_content "Title can't be blank"
-    expect(page).to have_content "Body can't be blank"
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
+
+    scenario 'asks a question with errors' do
+      click_on 'Ask'
+
+      expect(page).to have_content "Title can't be blank"
+      expect(page).to have_content "Body can't be blank"
+    end
   end
 
   scenario 'Not authenticated user asks a question' do
     visit questions_path
     click_on 'New question'
-
+    
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
