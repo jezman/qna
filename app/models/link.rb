660143d3ff896c276dd81d554b047e0adbec0c08
jezman <dev.jezman@gmail.com>
2018-12-19 12:21:12 +0300

    Add: Links url validation.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,8 @@
 class Link < ApplicationRecord
+  URL_FORMAT = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
+  validates :url, format: { with: URL_FORMAT, message:'invalid format' }
 end
