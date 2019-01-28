4b9927760169361a3ab76fbbbd463270ec0d6826
jezman <dev.jezman@gmail.com>
2019-01-28 15:26:48 +0300

    Add gem CanCanCan and ability for models.

---
+++ app/models/ability.rb
@@ -0,0 +1,48 @@
+class Ability
+  include CanCan::Ability
+
+  attr_reader :user
+
+  def initialize(user)
+    @user = user
+
+    if user
+      user.admin? ? admin_abilities : user_abilities
+    else
+      guest_abilities
+    end
+  end
+
+  def guest_abilities
+    can :read, :all
+  end
+
+  def admin_abilities
+    can :manage, :all
+  end
+
+  def user_abilities
+    guest_abilities
+    can :create, [Question, Answer, Comment]
+    can :update, [Question, Answer, Comment], user_id: user.id
+    can :destroy, [Question, Answer], user_id: user.id
+
+    can :destroy, ActiveStorage::Attachment do |resource|
+      user.author?(resource.record)
+    end
+
+    can :destroy, Link do |resource|
+      user.author?(resource.linkable)
+    end
+
+    can :best, Answer do |resource|
+      user.author?(resource.question)
+    end
+
+    can [:vote_up, :vote_down], [Question, Answer]
+    cannot [:vote_up, :vote_down], [Question, Answer], user_id: user.id
+    can :revoke, [Question, Answer] do |resource|
+      resource.likes.find_by(user_id: user.id)
+    end
+  end
+end
---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,88 @@
+require 'rails_helper'
+require 'cancan/matchers'
+
+describe Ability, type: :model do
+  include ControllerHelpers
+
+  subject(:ability) { Ability.new(user) }
+
+  describe 'for guest' do
+    let(:user) { nil }
+
+    it { should be_able_to :read, Question }
+    it { should be_able_to :read, Answer }
+    it { should be_able_to :read, Comment }
+
+    it { should_not be_able_to :manage, :all }
+  end
+
+  describe 'for admin' do
+    let(:user) { create :user, admin: true }
+
+    it { should be_able_to :manage, :all }
+  end
+
+  describe 'for user' do
+    let(:user) { create(:user) }
+    let(:other) { create(:user) }
+
+    let(:question) { create(:question, user: user) }
+    let(:question_other) { create(:question, user: other) }
+
+    let(:answer) { create(:answer, question: question, user: user) }
+    let(:answer_other) { create(:answer, question: question, user: other) }
+    let(:answer_other) { create(:answer, question: question_other, user: other) }
+
+    let(:comment) { create(:comment, commentable: question, user: user) }
+    let(:comment_other) { create(:comment, commentable: question, user: other) }
+
+    let(:link) { create(:link, linkable: question) }
+    let(:link_other) { create(:link, linkable: question_other) }
+
+    let!(:like_question) { create(:like, likable: question, user: user) }
+    let!(:like_question_other) { create(:like, likable: question_other, user: other) }
+    let!(:like_answer) { create(:like, likable: answer_other, user: user) }
+    let!(:like_answer_other) { create(:like, likable: answer, user: other) }
+
+    before do
+      attach_file_to(question)
+      attach_file_to(question_other)
+    end
+
+    it { should_not be_able_to :manage, :all }
+
+    it { should be_able_to :read, :all }
+
+    it { should be_able_to :create, Question }
+    it { should be_able_to :create, Answer }
+    it { should be_able_to :create, Comment }
+
+    it { should be_able_to :update, question }
+    it { should be_able_to :update, answer }
+    it { should be_able_to :update, comment, user: user }
+    it { should_not be_able_to :update, question_other }
+    it { should_not be_able_to :update, answer_other }
+    it { should_not be_able_to :update, comment_other, user: user }
+
+    it { should be_able_to :destroy, question, user: user }
+    it { should be_able_to :destroy, answer, user: user }
+    it { should be_able_to :destroy, link, user: user }
+    it { should be_able_to :destroy, question.files.last, user: user }
+    it { should_not be_able_to :destroy, question_other, user: user }
+    it { should_not be_able_to :destroy, answer_other, user: user }
+    it { should_not be_able_to :destroy, link_other, user: user }
+    it { should_not be_able_to :destroy, question_other.files.last, user: user }
+
+    it { should be_able_to [:vote_up, :vote_down], question_other, user: user }
+    it { should be_able_to [:vote_up, :vote_down], answer_other, user: user }
+    it { should be_able_to :revoke, question, user: user }
+    it { should be_able_to :revoke, answer_other, user: user }
+    it { should_not be_able_to [:vote_up, :vote_down], question, user: user }
+    it { should_not be_able_to [:vote_up, :vote_down], answer, user: user }
+    it { should_not be_able_to :revoke, question_other, user: user }
+    it { should_not be_able_to :revoke, answer, user: user }
+
+    it { should be_able_to :best, answer, user: user }
+    it { should_not be_able_to :best, answer_other, user: user }
+  end
+end
